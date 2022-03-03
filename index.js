e19063634c04d32007396a1aea7afe231fcdf312
Jobin Jacob Kavalam <jjkavalam@gmail.com>
2022-03-03 11:12:04 +0530

    add test suite

--- index.js
+++ index.js
@@ -1,125 +1,128 @@
 function parseCsv(s) {
+  function log(...args) {
+    // console.log("[debug]", ...args);
+  }
 
-    function log(...args) {
-        if (false)
-            console.log("[debug]", ...args)
+  function advance(n = 1) {
+    i = i + n;
+    l1 = s[i] || "";
+    l2 = s[i + 1] || "";
+    log("-- next", l1, l2);
+  }
+
+  let i = -1;
+  let l1, l2;
+  advance();
+
+  function expect(c) {
+    if (l1 !== c) {
+      error("expected '" + c + "' instead got: '" + l1 + "'");
     }
+  }
 
-    function advance(n = 1) {
-        i = i + n
-        l1 = s[i] || ''
-        l2 = s[i + 1] || ''
-        log("-- next", l1, l2)
+  function error(msg) {
+    throw new Error(
+      "Parse error: " +
+        "At position: " +
+        i +
+        ", '" +
+        s.substring(i, i + 10) +
+        "' Parse error: " +
+        msg
+    );
+  }
+
+  // ---
+
+  function acceptDoc() {
+    log("doc", l1);
+    acceptFieldset();
+    if (l1 !== "") {
+      acceptNl();
+      rows.push([]);
+      acceptDoc();
     }
+  }
 
-    let i = -1
-    let l1, l2
-    advance()
-
-    function expect(c) {
-        if (l1 !== c) {
-            error("expected '" + c + "' instead got: '" + l1 + "'")
-        }
+  function acceptFieldset() {
+    log("fieldset", l1);
+    acceptField();
+    if (l1 !== "" && l1 !== "\n") {
+      acceptComma();
+      acceptFieldset();
     }
+  }
 
-    function error(msg) {
-        throw new Error('Parse error: ' + "At position: " + i + ", '" + s.substring(i, i + 10) + "' Parse error: " + msg)
+  function acceptField() {
+    log("field", l1);
+    if (l1 === '"') {
+      acceptQuotedString();
+    } else {
+      acceptUnquotedString();
     }
+  }
 
-    // ---
+  function acceptQuotedString() {
+    log("quotedString", l1);
+    acceptQuote();
+    // keep scanning until next lone quote is found
+    acceptRestOfQuotedString();
+  }
 
-    function acceptDoc() {
-        log("doc", l1)
-        acceptFieldset()
-        if (l1 !== '') {
-            acceptNl()
-            acceptDoc()
-        }
+  function acceptNl() {
+    log("nl", l1);
+    expect("\n");
+    advance();
+  }
+
+  function acceptComma() {
+    log("comma", l1);
+    expect(",");
+    advance();
+  }
+
+  function acceptQuote() {
+    log("quote", l1);
+    expect('"');
+    advance();
+  }
+
+  function acceptRestOfQuotedString() {
+    log("rest", l1);
+    // keep moving forward until a lone quote is reached
+    // expect
+    let stop = false;
+    let start = i;
+    while (!stop) {
+      if (l1 === "") {
+        error("unexpectedly reached eof");
+      } else if (l1 === '"' && l2 === '"') {
+        // skip
+        advance();
+      } else if (l1 === '"') {
+        stop = true;
+      }
+      // advance
+      advance();
     }
+    rows[rows.length - 1].push(s.substring(start, i - 1));
+  }
 
-    function acceptFieldset() {
-        log("fieldset", l1)
-        rows.push([])
-        acceptField()
-        if (l1 !== '' && l1 !== '\n') {
-            acceptComma()
-            acceptFieldset()
-        }
-    }
+  function acceptUnquotedString() {
+    log("unquoted", l1);
+    // expect
+    // until next (comma|newline|quote|EOF)
+    const m = s.substring(i).match(/([,\n"]|$)/);
+    if (!m) error("expected unquoted string");
+    rows[rows.length - 1].push(s.substring(i, i + m.index));
+    advance(m.index);
+  }
 
-    function acceptField() {
-        log("field", l1)
-        if (l1 === '"') {
-            acceptQuotedString()
-        } else {
-            acceptUnquotedString()
-        }
-    }
-
-    function acceptQuotedString() {
-        log("quotedString", l1)
-        acceptQuote()
-        // keep scanning until next lone quote is found
-        // X(?!Y) X if not followed by Y
-        acceptRestOfQuotedString()
-    }
-
-    function acceptNl() {
-        log("nl", l1)
-        expect('\n')
-        advance()
-    }
-
-    function acceptComma() {
-        log("comma", l1)
-        expect(',')
-        advance()
-    }
-
-    function acceptQuote() {
-        log("quote", l1)
-        expect('"')
-        advance()
-    }
-
-
-    function acceptRestOfQuotedString() {
-        log("rest", l1)
-        // keep moving forward until a lone quote is reached
-        // expect
-        let stop = false;
-        let start = i
-        while (!stop) {
-            if (l1 === '') {
-                error("unexpectedly reached eof")
-            } else if (l1 === '"' && l2 === '"') {
-                // skip
-                advance()
-            } else if (l1 === '"') {
-                stop = true;
-            }
-            // advance
-            advance()
-        }
-        rows[rows.length - 1].push(s.substring(start, i - 1))
-    }
-
-
-    function acceptUnquotedString() {
-        log("unquoted", l1)
-        // expect
-        // until next (comma|newline|quote|EOF)
-        const m = s.substring(i).match(/([,\n"]|$)/)
-        if (!m) error("expected unquoted string")
-        rows[rows.length - 1].push(s.substring(i, i + m.index))
-        advance(m.index)
-    }
-
-    let rows = []
-    acceptDoc()
-    return rows
+  let rows = [[]];
+  acceptDoc();
+  return rows;
 }
 
 module.exports = {
-    parseCsv
-}
+  parseCsv,
+};
--- test/parse.test.js
+++ test/parse.test.js
@@ -8,4 +8,19 @@ describe("parsing", function () {
   it("parses single record", function () {
     expect(parseCsv("a,b,c")).to.deep.equal([["a", "b", "c"]]);
   });
+  it("parses multiple records", function () {
+    expect(parseCsv("a,b,c\nd,e")).to.deep.equal([
+      ["a", "b", "c"],
+      ["d", "e"],
+    ]);
+  });
+  it("parses quoted fields", function () {
+    expect(parseCsv('a,"b",c')).to.deep.equal([["a", "b", "c"]]);
+  });
+  it("parses multiline fields", function () {
+    expect(parseCsv('a,"b1\nb2",c')).to.deep.equal([["a", "b1\nb2", "c"]]);
+  });
+  it("parses escaped quotes", function () {
+    expect(parseCsv('a,"b""",c')).to.deep.equal([["a", 'b""', "c"]]);
+  });
 });
